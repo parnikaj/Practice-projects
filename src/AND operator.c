2f8c7bbf1105031d9d99b32a3d78b22524c49c87
parnikaj <97286661+parnikaj@users.noreply.github.com>
2022-10-17 21:31:11 +0400

    Add files via upload

---
+++ src/AND operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+
+float grade = 87; 
+if(grade >= 0 && grade <= 35){ 
+printf("\nYou have failed this test!"); 
+}
+else{
+printf("\nYou have passed this test!"); 
+}
+   return 0;
+}

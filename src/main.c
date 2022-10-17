c67f874b03e5fb939be55e483c1bde2e722a2b68
Parnika Jain <parnikajain@Navneets-Air.lan>
2022-10-17 21:24:52 +0400

    AND oprrator

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <math.h>
+
 int main(){
-    printf("hello world"); //hello world program 
-    return 0; 
+
+const double PI = 3.14159; 
+double radius; 
+double circumference; 
+
+printf("\nRadius of the circle:"); 
+scanf("%lf",&radius); 
+
+circumference = PI * 2 * radius; 
+
+printf("\n:The circumference is:%lf",circumference);
+
+  return 0; 
 }
